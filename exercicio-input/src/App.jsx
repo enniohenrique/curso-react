aa88faaa16fa41fcd7abb04b6db9e436d48b9307
Ennio <ennioh@icloud.com>
2023-07-19 18:33:46 -0400

    Commit de teste

--- exercicio-input/src/App.jsx
+++ exercicio-input/src/App.jsx
@@ -1,48 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-  });
+  const [form, setForm] = React.useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(event);
-  }
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    console.log(id, value);
-    setForm({ ...form, [id]: value });
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        type="text"
-        name="nome"
-        value={form.nome}
-        onChange={handleChange}
-      />
-
-      {form.nome}
-
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      {form.email}
-
-      <button>Enviar</button>
-    </form>
-  );
+  return <form>
+    <label htmlFor=""></label>
+    <input type="text" />
+  </form>;
 };
 
 export default App;
