73e6896cf5d5aa398edf71e946dfdc382d75298a
Ennio <ennioh@icloud.com>
2023-07-24 22:33:51 -0400

    Aula sobre componentes Radios

---
+++ 0408-componentes-radio/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Input from './Form/Input';
+import Select from './Form/Select';
+import Radio from './Form/Radio';
+
+const App = () => {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('Vermelho');
+  const [frutas, setFrutas] = React.useState('Vermelho');
+
+  return (
+    <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limao', 'Laranja', 'Uva']}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
+      <Select
+        options={['smartphone', 'tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
