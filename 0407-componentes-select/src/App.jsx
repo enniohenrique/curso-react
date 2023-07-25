f70a20cfdbb692994c00ab3e7ac80da78c2637b4
Ennio <ennioh@icloud.com>
2023-07-24 22:21:58 -0400

    Aula de Componentes de select

---
+++ 0407-componentes-select/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Input from './Form/Input';
+import Select from './Form/Select';
+
+const App = () => {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+
+  return (
+    <form>
+      <Select options={['smartphone', 'tablet']} value={produto} setValue={setProduto} />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
